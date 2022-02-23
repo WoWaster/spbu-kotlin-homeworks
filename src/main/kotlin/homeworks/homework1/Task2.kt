c3fbfb5fb6e7f11a3ad6350ed20b115e4782b6c2
WoWaster <wowasterdev@gmail.com>
2022-02-23 16:18:38 +0300

    Improve UX

--- src/main/kotlin/homeworks/homework1/Task2.kt
+++ src/main/kotlin/homeworks/homework1/Task2.kt
@@ -1,5 +1,7 @@
 package homeworks.homework1
 
+import kotlin.system.exitProcess
+
 fun sieveOfEratosthenes(bound: Int): IntArray {
     val numbers = BooleanArray(bound + 1) { true }
     numbers[0] = false
@@ -28,9 +30,15 @@ fun sieveOfEratosthenes(bound: Int): IntArray {
 }
 
 fun main() {
-    println("This app will print every prime below given.")
-    print("Please enter n: ")
+    println("This app will print every prime below given number.")
+    print("Please enter number: ")
     val number = readln().toInt()
+
+    if (number < 2) {
+        println("There are no primes below $number.")
+        exitProcess(0)
+    }
+
     val primes = sieveOfEratosthenes(number)
     print("All primes below $number: ")
     for (prime in primes)
