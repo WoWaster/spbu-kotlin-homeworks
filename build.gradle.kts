406ae44f242094d798c38bfdb4ef227ff243f193
WoWaster <wowasterdev@gmail.com>
2022-05-20 19:48:50 +0300

    Add compose and bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,34 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("io.gitlab.arturbosch.detekt").version("1.19.0")
+    kotlin("jvm") version "1.6.21"
+    id("io.gitlab.arturbosch.detekt").version("1.20.0")
+    id("org.jetbrains.compose")
 }
 
 group = "me.wowaster"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
+detekt {
+    config = files("detekt-config.yml")
+}
+
+@OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
 dependencies {
     implementation(kotlin("stdlib"))
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.20.0")
     testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(compose.desktop.currentOs)
+    implementation(compose.materialIconsExtended)
+    implementation(compose.material3)
+    implementation("io.ktor:ktor-client-core:2.0.1")
+    implementation("io.ktor:ktor-client-cio:2.0.1")
+    implementation("org.jsoup:jsoup:1.15.1")
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
 rootProject.name = "spbu-kotlin-homeworks"
 
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
+}
